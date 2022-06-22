3a95c366c0d061bc3002fc31d15ea3b291c5936e
czyan <czyang2002@gmail,com>
2022-06-22 11:57:09 +0800

    asdasasda

--- pages/sike.vue
+++ pages/sike.vue
@@ -21,13 +21,28 @@
           You are the {{ count }}{{ nth(count) }} person to click on this link
         </p>
         <p class="mt-16 text-xl text-block text-gray-500">
-          Do await our Battalion Digital Defense Activity, which us coming on
-          27th June, A real Dissemination message will come out then! The
-          activity will cover various cyber safety guidelines, including, how to
-          avoid phishing scams, how to select a good password ect. <br />
-          See you there!
+          Fell for the link? No problem! To avoid falling for such scams in the
+          future, do lookout for our Battalion's very own <p class="font-bold">#LetsPlayItCYBERSafe</p>
+          minigame which will come with a dissemination message!
         </p>
       </div>
+      <p class="text-center text-xl text-block text-gray-500">
+        <br />
+        The minigame contains various fun features such as:
+        <br />
+        -Simulating signing up for a social media site
+        <br />
+        -Dealing with up to <p class="font-bold">15 Unique, Informative scenarios</p> inspired by real
+        life events
+        <br />
+        and many more!
+        <div class="text-center">
+          <br />
+          <p class="font-bold">Launching 27th June to 4th July,</p>
+          <br />
+          3rd Transport, LetsPlayItCYBERSafe!
+        </div>
+      </p>
     </div>
   </div>
 </template>
